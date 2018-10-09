8634af0c639774bf78506f3b948769f11e9ce967
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-10-09 20:32:28 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,6 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where('rating = ?', highest_rating).flatten
+    Show.where('rating = ?', highest_rating)[0][0]
   end
 end
